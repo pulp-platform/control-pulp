a398873944cb3a850762b319ed9d98924746ea22
bluew <bluewww@users.noreply.github.com>
2023-05-23 21:38:08 +0200

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,33 @@
+;; Copyright 2023 ETH Zurich and University of Bologna
+;;
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;;     http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+;;
+;; SPDX-License-Identifier: Apache-2.0
+;;
+;; Robert Balas <balasr@iis.ee.ethz.ch>
+;; Alessandro Ottaviano<aottaviano@iis.ee.ethz.ch>
+
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((fill-column . 80)))
+ (verilog-mode
+  ;; (verilog-indent-lists quote nil)
+  ;; (verilog-auto-lineup quote all)
+  (verilog-cexp-indent . 2)
+  (verilog-case-indent . 2)
+  (verilog-indent-level-behavioral . 2)
+  (verilog-indent-level-declaration . 2)
+  (verilog-indent-level-module . 2)
+  (verilog-indent-level . 2)))
+
